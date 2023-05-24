e200b98385a132b39f80a4e30a11dbb397113d0f
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-05-24 11:34:17 -0500

    Performance

--- data/Movies.js
+++ data/Movies.js
@@ -1,4 +1,7 @@
+'use strict';
+
 const axios = require('axios');
+const cache = require('./cache.js');
 
 class Movie {
   constructor(title, overview, releaseDate) {
@@ -8,17 +11,31 @@ class Movie {
   }
 }
 
-async function getMovies(searchQuery) {
-  try {
-    let movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${process.env.MOVIE_API_KEY}`);
+async function getMovies(searchQuery, city) {
+  const key = 'movies-' + searchQuery + '-' + city;
+  const url = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${process.env.MOVIE_API_KEY}`;
 
-    let localMovies = movieData.data.results.map(movie => {
+  if (cache[key] && (Date.now() - cache[key].timestamp < 120000)) {
+    console.log('Cache hit');
+    return cache[key].data;
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(url)
+      .then(response => parseMovies(response.data));
+    return cache[key].data;
+  }
+}
+
+function parseMovies(movieData) {
+  try {
+    const localMovies = movieData.results.map(movie => {
       return new Movie(movie.title, movie.overview, movie.release_date);
     });
-
-    return localMovies;
+    return Promise.resolve(localMovies);
   } catch (error) {
-    throw new Error(error.message);
+    return Promise.reject(error);
   }
 }
 
--- data/Weather.js
+++ data/Weather.js
@@ -1,4 +1,7 @@
+'use strict';
+
 const axios = require('axios');
+const cache = require('./cache.js');
 
 class Forecast {
   constructor(date, description) {
@@ -7,17 +10,31 @@ class Forecast {
   }
 }
 
-async function getWeather(lat, lon) {
-  try {
-    let weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
+async function getWeather(latitude, longitude) {
+  const key = 'weather-' + latitude + longitude;
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${latitude}&lon=${longitude}&days=5`;
 
-    let dailyForecasts = weatherData.data.data.map(day => {
+  if (cache[key] && (Date.now() - cache[key].timestamp < 120000)) {
+    console.log('Cache hit');
+    return cache[key].data;
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(url)
+      .then(response => parseWeather(response.data));
+    return cache[key].data;
+  }
+}
+
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
       return new Forecast(day.datetime, day.weather.description);
     });
-
-    return dailyForecasts;
-  } catch (error) {
-    throw new Error(error.message);
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
   }
 }
 
